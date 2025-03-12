9863669de27c9b5058b0686cb1605c8713630fc3
subin1848 <s2315@e-mirim.hs.kr>
2025-03-12 12:13:53 +0900

    0312 forEach, for-of

---
+++ grammar/for.js
@@ -0,0 +1,13 @@
+const letter = ['a', 'b', 'c', 'd'];
+
+for(let i = 0; i < letter.length; i++){
+    console.log(letter[i]);
+}
+
+// letter.forEach(function(f){
+//     console.log(f);
+// });
+letter.forEach(f => console.log(f));
+
+for(const f of letter)
+    console.log(f)
