8efa4cf3f9025c43ea8119459e6b5a496924184f
subin1848 <s2315@e-mirim.hs.kr>
2025-03-12 11:44:00 +0900

    0312 arrow function 3

--- grammar/function.js
+++ grammar/function.js
@@ -6,7 +6,7 @@ test('a');
 
 // arrow function 
 // 매개변수가 하나인 경우 소괄호()를 없앤다
-testAn = text => {
-    console.log('testAn' + text);
-}
+// 함수 body가 return문 하나이거나, 한 줄 일 때 중괄호{}를 없앤다 
+testAn = text => console.log('testAn' + text);
+
 testAn('arrow function');
