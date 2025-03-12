d6157f88562d5bc2ea5f0489889358727e33fdfb
subin1848 <s2315@e-mirim.hs.kr>
2025-03-12 11:41:31 +0900

    0312 arrow function 2

--- grammar/function.js
+++ grammar/function.js
@@ -4,8 +4,9 @@ function test(text){
 }
 test('a');
 
-// arrow function
-testAn = (text) => {
+// arrow function 
+// 매개변수가 하나인 경우 소괄호()를 없앤다
+testAn = text => {
     console.log('testAn' + text);
 }
 testAn('arrow function');
