11e7990820cb05c524d104df89098472dbacf28f
subin1848 <s2315@e-mirim.hs.kr>
2025-04-01 10:22:23 +0900

    0401 DB에서 READ

--- app2.js
+++ app2.js
@@ -12,8 +12,6 @@ app.set('view engine', 'ejs');
 // path.join() : 경로 지정자( \ or / )를 운영체제와 맞추어 줌
 app.set('views', path.join(__dirname, 'views'));
 
-const travelList = ['뉴욕', '파리', '서울', '도쿄'];
-
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -34,8 +32,17 @@ app.get('/', (req, res) => {
 })
 
 app.get('/travel', (req, res) => {
-    res.render('travel', {travelList});
-})
+    const _query = 'SELECT id, name FROM travellist';
+    db.query(_query, (err, results) => {
+        if(err){
+            console.error('데이터 쿼리 실패 : ', arr);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+        const travelList = results;
+        res.render('travel', {travelList});
+    });
+});
 
 app.use((req, res) => {
 
