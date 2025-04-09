3a3a902fbde6aded816adfdc2119862870fe7d19
subin1848 <s2315@e-mirim.hs.kr>
2025-04-09 11:12:40 +0900

    DELETE

--- app2.js
+++ app2.js
@@ -13,8 +13,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
-// __dirname 현재 디렉토리의 절대 경로 (C: 까지 나옴)
-// path.join() : 경로 지정자( \ or / )를 운영체제와 맞추어 줌
 app.set('views', path.join(__dirname, 'views'));
 
 const db = mysql.createConnection({
@@ -98,6 +96,7 @@ app.put('/travel/:id', (req, res) =>{
     }); 
 });
 
+
 app.get('/travel/:id/edit', (req, res) =>{
     const travelId = req.params.id;
     const _query = 'SELECT * FROM travellist WHERE id = ?';
@@ -115,6 +114,20 @@ app.get('/travel/:id/edit', (req, res) =>{
         res.render('editTravel', {travel});
     });
 });
+
+app.delete('/travel/:id', (req, res) =>{
+    const travelId = req.params.id;
+    const _query = 'DELETE FROM travellist WHERE id = ?';
+    db.query(_query, [travelId], (err, results) => {
+        if(err) {
+            console.error('데이터베이스 쿼리 실패 : ', err);
+            res.status(500).send('데이터베이스 에러');
+            return;
+        };
+        res.render('deleteSuccess');
+    }); 
+});
+
 
 app.use((req, res) => {
 
