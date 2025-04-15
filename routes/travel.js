f61ebcd0352eeb0c497a90ccdf70e204bf6bbd2f
subin1848 <s2315@e-mirim.hs.kr>
2025-04-15 09:40:29 +0900

    routing 순서 정리

--- routes/travel.js
+++ routes/travel.js
@@ -2,16 +2,31 @@ const express = require('express');
 const router = express.Router();
 const db = require('../dbConnection');
 
-// routing 설정
+// 게시글의 전체 목록
+router.get('/', (req, res) => {
+    const _query = 'SELECT id, name FROM travellist';
+    db.query(_query, (err, results) => {
+        if (err) {
+            console.error('데이터 쿼리 실패 : ', arr);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+        const travelList = results;
+        res.render('travel', { travelList });
+    });
+});
+
+// 게시글 추가 페이지로 이동
 router.get('/add', (req, res) => {
     res.render('addTravel');
 });
 
+// 게시글을 추가
 router.post('/', (req, res) => {
     const { name } = req.body;
     const _query = 'INSERT INTO travellist (name) VALUE (?)';
-    db.query(_query, [name], (err,results) => {
-        if(err) {
+    db.query(_query, [name], (err, results) => {
+        if (err) {
             console.error('데이터베이스 쿼리 실패: ', err);
             res.status(500).send('Internal Server Error');
             return;
@@ -20,80 +35,70 @@ router.post('/', (req, res) => {
     });
 });
 
-router.get('/', (req, res) => {
-    const _query = 'SELECT id, name FROM travellist';
-    db.query(_query, (err, results) => {
-        if(err){
-            console.error('데이터 쿼리 실패 : ', arr);
-            res.status(500).send('Internal Server Error');
-            return;
-        }
-        const travelList = results;
-        res.render('travel', {travelList});
-    });
-});
-
+// 해당 게시글 내용 읽기
 router.get('/:id', (req, res) => {
     const travelId = req.params.id;
     const query = 'SELECT * FROM travellist WHERE id = ?';
-    db.query(query, [travelId], (err, results) =>{
-        if(err) {
+    db.query(query, [travelId], (err, results) => {
+        if (err) {
             console.error('데이터베이스 쿼리 실패', err);
             res.status(500).send('Internal Server Error');
-            return;    
+            return;
         }
-        if(results.length === 0) {
+        if (results.length === 0) {
             res.status(404).send('여행지를 찾을 수 없습니다.');
             return;
         }
         const travel = results[0];
-        res.render('travelDetail', {travel});
+        res.render('travelDetail', { travel });
     });
 });
 
-router.put('/:id', (req, res) =>{
+// 해당 게시글 수정 페이지로 이동
+router.get('/:id/edit', (req, res) => {
+    const travelId = req.params.id;
+    const _query = 'SELECT * FROM travellist WHERE id = ?';
+    db.query(_query, [travelId], (err, results) => {
+        if (err) {
+            console.error('데이터베이스 쿼리 실패', err);
+            res.status(500).send('Internal Server Error');
+            return; 
+        }
+        if (results.length === 0) {
+            res.status(404).send('여행지를 찾을 수 없습니다.');
+            return;
+        }
+        const travel = results[0];
+        res.render('editTravel', { travel });
+    });
+});
+
+// 게시글 수정
+router.put('/:id', (req, res) => {
     const travelId = req.params.id;
     const { name } = req.body;
     const _query = 'UPDATE travellist SET name=? WHERE id = ?';
     db.query(_query, [name, travelId], (err, results) => {
-        if(err) {
+        if (err) {
             console.error('데이터베이스 쿼리 실패 : ', err);
             res.status(500).send('데이터베이스 에러');
             return;
         };
         res.render('updateSuccess');
-    }); 
-});
-
-
-router.get('/:id/edit', (req, res) =>{
-    const travelId = req.params.id;
-    const _query = 'SELECT * FROM travellist WHERE id = ?';
-    db.query(_query, [travelId], (err, results) =>{
-        if(err) {
-            console.error('데이터베이스 쿼리 실패', err);
-            res.status(500).send('Internal Server Error');
-            return;    
-        }
-        if(results.length === 0) {
-            res.status(404).send('여행지를 찾을 수 없습니다.');
-            return;
-        }
-        const travel = results[0];
-        res.render('editTravel', {travel});
     });
 });
 
-router.delete('/:id', (req, res) =>{
+// 게시글 삭제
+router.delete('/:id', (req, res) => {
     const travelId = req.params.id;
     const _query = 'DELETE FROM travellist WHERE id = ?';
     db.query(_query, [travelId], (err, results) => {
-        if(err) {
+        if (err) {
             console.error('데이터베이스 쿼리 실패 : ', err);
             res.status(500).send('데이터베이스 에러');
             return;
         };
         res.render('deleteSuccess');
-    }); 
+    });
 });
 module.exports = router;
